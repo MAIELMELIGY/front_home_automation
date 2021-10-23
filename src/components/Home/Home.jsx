84bbabb8dbaf012564d5f40d8522cf355599f560
MAIELMELIGY <42947543+MAIELMELIGY@users.noreply.github.com>
2021-10-23 11:08:04 +0200

    Add files via upload

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,45 +11,26 @@ import Client from "../Client/client";
 import AboutNumber from "../About/about_number";
 import Navbar from "./../Navbar/Navbar";
 import Category from "../Product/category";
+
 import video from "../../img/cover.mp4";
 import VideoLooper from "react-video-looper";
 import Project from "../projects/projects";
 class Home extends React.Component {
-  state = {
-    home: [],
-  };
-  componentDidMount() {
-    axios
-      .get("https://apihomeautomation.herokuapp.com/api/Header/")
-      .then((response) => {
-        this.setState({ home: response.data });
-      })
-
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
   render() {
     return (
       <div id="main">
         <Navbar />
-        <div className="slider">
-          {this.state.home.map((home) => {
-            return (
-              <div>
-                <div
-                  style={{
-                    width: "1536px",
-                    height: "600px",
-                    overflow: "hidden",
-                     allow="autoplay"
-                  }}
-                >
-                  <VideoLooper source={video} />
-                </div>
-              </div>
-            );
-          })}
+        <div>
+          <div
+            className="cover"
+            style={{
+              width: "1536px",
+              height: "600px",
+              overflow: "hidden",
+            }}
+          >
+            <VideoLooper source={video} autoPlay={true} muted={true} />
+          </div>
         </div>
 
         <Service />
